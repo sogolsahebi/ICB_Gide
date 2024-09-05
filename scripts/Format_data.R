a9485146d0ecf4b7f0a94415077ac299cf2ec0f6
Nasim Bondar Sahebi <113414219+sogolsahebi@users.noreply.github.com>
2024-09-05 09:52:06 -0400

    Add files via upload

---
+++ scripts/Format_data.R
@@ -0,0 +1,115 @@
+# Format_Data Processing 
+# Format_EXPR.R
+# Goal: 
+# - Creates "CLIN.txt" with dimensions 91 x 38
+# - Creates cased_sequenced.csv: dimensions 91 x 4
+# - Creates EXPR_gene_tpm.csv, EXPR_gene_counts.csv, EXPR_tx_tpm.csv, EXPR_tx_counts.csv 
+#   - EXPR_gene_tpm.csv: dimensions 61,544 x 91
+#   - EXPR_gene_counts.csv: dimensions 61,544 x 91
+#   - EXPR_tx_tpm.csv: dimensions 246,624 x 91
+#   - EXPR_tx_counts.csv: dimensions 246,624 x 91
+
+# Libraries and Source Files
+# Access necessary functions from ICB_common codes.
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/annotate_tissue.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/annotate_drug.R")
+
+library(data.table)
+library(R.utils)
+library(stringr)
+library(tibble)
+
+# args <- commandArgs(trailingOnly = TRUE)
+# input_dir <- args[1]
+# output_dir <- args[2]
+# annot_dir <- args[3]
+
+## 1. Get Clinical Data (CLIN.csv)
+clin_orginal <- read.csv("files/CLIN.txt", stringsAsFactors=FALSE, sep="\t", dec=',') # 91 x 25
+
+selected_cols <- c("patient" , "Age..Years." , "Sex" , "Treatment" , "Best.RECIST.response" , 
+                   "Progression.Free.Survival..Days." , "Overall.Survival..Days." , "Progressed" , "Last.Followup.Status")
+
+clin <- cbind(clin_orginal[, selected_cols], "Melanoma", NA, NA, NA, NA, NA, NA, NA, NA)
+
+# Reorder columns
+colnames(clin) <- c( "patient" , "age" , "sex" , "drug_type" , "recist" , "t.pfs" , "t.os" , "pfs" ,"os" ,
+                     "primary" , "histo" , "response" , "stage" , "response.other.info" , "dna" ,"dna_info", "rna", "rna_info")
+
+# Set rna and rna_info
+clin$rna <- "rnaseq"
+clin$rna_info <- "tpm"
+
+# Set pfs and os, and convert OS days into months, ensuring two decimal points
+clin$pfs <- as.numeric(as.character(ifelse(clin$pfs %in% "Yes", 1, ifelse(clin$pfs %in% "No", 0, NA))))
+clin$t.pfs <- format(as.numeric(as.character(clin$t.pfs)) / 30)
+clin$t.os <- format(as.numeric(as.character(clin$t.os)) / 30)
+
+# Set os as binary representation
+clin$os <- ifelse(clin$os %in% "Alive", 0, ifelse(clin$os %in% c("Dead", "Dead, melanoma"), 1, 0))
+
+# Calculate the response using Get_Response function
+clin$response <- Get_Response(data = clin)
+
+# Reorder columns
+clin <- clin[, c(
+  "patient", "sex", "age", "primary", "histo", "stage", 
+  "response.other.info", "recist", "response", "drug_type", "dna", "dna_info", "rna", "rna_info", "t.pfs", 
+  "pfs", "t.os", "os"
+)]
+
+# Use the format_clin_data function for further formatting
+clin <- format_clin_data(clin_orginal, "patient", selected_cols, clin)
+
+# Read 'curation_tissue.csv' file
+path <- "https://raw.githubusercontent.com/BHKLAB-DataProcessing/ICB_Common/main/data/curation_tissue.csv"
+annotation_tissue <- read.csv(path)
+
+# Annotate 'clin' using the 'annotation_tissue' data; Set tissueid column (survival_unit and survival_type columns are added in this step)
+clin <- annotate_tissue(clin=clin, study='Gide', annotation_tissue=annotation_tissue, check_histo=FALSE) 
+
+# Set treatmentid after tissueid column, based on curation_drug.csv file
+annotation_drug <- read.csv("https://raw.githubusercontent.com/BHKLAB-DataProcessing/ICB_Common/main/data/curation_drug.csv")
+clin <- add_column(clin, treatmentid=annotate_drug('Gide', clin$drug_type, annotation_drug), .after='tissueid')
+
+# Six categories: PD-1/PD-L1, CLA4, IO+combo, IO+chemo, Chemo+targeted, targeted, IO+targeted
+clin$drug_type[clin$treatmentid == "Nivolumab" | clin$treatmentid == "Pembrolizumab"] <- 'PD-1/PD-L1'
+clin$drug_type[clin$treatmentid == "Ipilimumab + Pembrolizumab" | clin$treatmentid == "Ipilimumab + Nivolumab"] <- 'IO+combo'
+
+# Replace empty string values with NA
+clin[clin == "-"] <- NA
+
+# Remove any semicolons or unwanted characters in the columns
+clin <- data.frame(lapply(clin, function(x) gsub(";", "", x)))
+
+# Save the processed data as CLIN.csv file
+write.table(clin, file="files/CLIN.csv", sep=";", quote=FALSE, col.names=TRUE, row.names=FALSE)
+
+
+## 2. Get EXPR and case files
+# Read expression data
+expr_list <- readRDS("files/expr_list.rds")
+
+# Patients should match between expression and clinical data
+patients <- intersect(colnames(data.frame(expr_list$expr_gene_tpm)), clin$patient)
+clin <- clin[clin$patient %in% patients, ]
+
+# Create the case file
+case <- data.frame(patient=patients, snv=0, cna=0, expr=1)
+colnames(case) <- c("patient", "snv", "cna", "expr")
+
+# Save the case file
+write.table(case, file="files/cased_sequenced.csv", sep=";", quote=FALSE, col.names=TRUE, row.names=FALSE)
+
+# Save the expression data
+for (assay_name in names(expr_list)) {
+  write.table(
+    expr_list[[assay_name]],
+    file=file.path("files/", paste0('EXPR_', str_replace(assay_name, 'expr_', ''), '.csv')),
+    quote=FALSE, sep=";", col.names=TRUE, row.names=TRUE
+  )
+}
+
+write.table( case , "files/cased_sequenced.csv" , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
---
+++ scripts/Format_downloaded_data.R
@@ -0,0 +1,76 @@
+# Format_downloaded_data.R
+
+# Formats and cleans clinical and expression data.
+# Outputs: 
+# - "CLIN.txt" (91 x ???)
+# - 'expr_list.rds' including:
+# - expr_gene_tpm: 61544 x 91
+# - expr_gene_counts: 61544 x 43
+# - expr_isoform_tpm: 246624 x 91
+# - expr_isoform_counts: 246624 x 91
+
+library(data.table)
+library(readxl) 
+library(stringr)
+library(tximport)
+
+# Clinical data
+clin_PD1 <- read_excel("files/1-s2.0-S1535610819300376-mmc2.xlsx")
+clin_IPIPD1 <- read_excel("files/1-s2.0-S1535610819300376-mmc3.xlsx")
+mapping <- read.csv("files/filereport_read_run_PRJEB23709_tsv.txt", sep = "\t")
+
+# Clean clinical data
+colnames(clin_PD1) <- clin_PD1[2, ]
+clin_PD1 <- clin_PD1[-c(1:2), ]
+colnames(clin_IPIPD1) <- clin_IPIPD1[2, ]
+clin_IPIPD1 <- clin_IPIPD1[-c(1:2), ]
+
+# Add "PD1_" and "ipiPD1_" prefixes
+clin_PD1$Patient.no. <- paste0("PD1_", seq_len(nrow(clin_PD1)))
+clin_IPIPD1$Patient.no. <- paste0("ipiPD1_", seq_len(nrow(clin_IPIPD1)))
+
+# Combine clinical data
+clin <- rbind(clin_PD1, clin_IPIPD1)
+
+# Extract Patient.no. from mapping
+mapping$Patient.no. <- sapply(strsplit(mapping$submitted_ftp, "/"), function(x) {
+  sub("(_PRE|_EDT).*", "", tail(x, n=1))
+})
+
+# Merge clinical and mapping data
+clin_merged <- merge(clin, mapping, by = "Patient.no.")
+colnames(clin_merged)[colnames(clin_merged) == "run_accession"] <- "patient"
+
+# Save clinical data
+write.table(clin_merged, "files/CLIN.txt", quote = FALSE, sep = "\t", row.names = FALSE)
+
+# Load Gencode annotation
+load("files/Gencode.v40.annotation.RData")
+
+# Process expression data (kallisto output)
+process_kallisto_output <- function(work_dir, tx2gene) {
+  samples <- list.dirs(file.path(work_dir, 'rnaseq'), full.names = FALSE, recursive = FALSE)
+  files <- file.path(work_dir, 'rnaseq', samples, "abundance.h5")
+  names(files) <- samples
+  
+  # Import expression data
+  expr_tx <- tximport(files, type = "kallisto", txOut = TRUE, ignoreAfterBar = TRUE)
+  expr_gene <- tximport(files, type = "kallisto", tx2gene = tx2gene, ignoreAfterBar = TRUE)
+  
+  # Prepare expression data lists
+  expr_list <- list(
+    expr_gene_tpm = log2(expr_gene$abundance + 0.001),
+    expr_gene_counts = log2(expr_gene$counts + 1),
+    expr_isoform_tpm = log2(expr_tx$abundance + 0.001),
+    expr_isoform_counts = log2(expr_tx$counts + 1)
+  )
+  
+  return(expr_list)
+}
+
+# Define the working directory and process the expression data
+work_dir <- "files/kallisto_v0.46.1_GRCh38.40"
+expr_list <- process_kallisto_output(work_dir, tx2gene)
+
+# Save the expression list as RDS
+saveRDS(expr_list, file = "files/expr_list.rds")
